de592744e685bb95d21c5e6aa76c44ea11e9c4da
zhangzongbao <zhangzongbao1992@163.com>
2018-02-23 18:14:54 +0800

    tagsApi

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,4 +1,6 @@
 import crypto from 'crypto'
+import errMsg from '../config.js'
+
 
 module.exports = {
     MD5_SUFFIX: 'zzb_blog',
@@ -6,11 +8,12 @@ module.exports = {
         let md5 = crypto.createHash('md5');
         return md5.update(pwd).digest('hex')
     },
-    responseClient(res,httpCode = 500, code = 3,message='服务端异常',data={}) {
+    responseClient(res,httpCode = 500, code = 3,message,result={}) {
         let responseData = {};
-        responseData.code = code;
-        responseData.message = message;
-        responseData.data = data;
+        responseData.errno = code;
+        responseData.errmsg = message || errMsg[code];
+        responseData.result = result;
         res.status(httpCode).json(responseData)
-    }
+    },
+    
 }
